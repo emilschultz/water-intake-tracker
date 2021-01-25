d0a605bbb534a292808045c47480e1a6aae61163
Emil Schultz <emilrschultz@gmail.com>
2021-01-25 14:50:18 +0100

    large cup updates based on how many cups of water the user has checked

--- script.js
+++ script.js
@@ -3,15 +3,42 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
+
 smallCups.forEach((cup, index) => {
   cup.addEventListener('click', () => highlightCups(index))
 });
 
 const highlightCups = (index) => {
   smallCups.forEach((cup, index2) => {
-    
+
+    index === 7 && smallCups[index].classList.contains('full') ? index-- : ""
 
     smallCups[index].classList.contains('full') && !smallCups[index].nextElementSibling.classList.contains('full') ? index-- : "";
 
     index2 <= index ? cup.classList.add('full') : cup.classList.remove('full')
-  })}
+  })
+  updateBigCup()
+}
+
+const updateBigCup = () => {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+  const totalCups = smallCups.length
+
+  if(fullCups === 0) {
+    percentage.style.visibility = 'hidden', percentage.style.height = 0
+    percentage.style.height = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${fullCups / totalCups * 330}px`
+    percentage.innerText = `${fullCups / totalCups * 100}%`
+  }
+
+  if(fullCups === totalCups) {
+    remained.style.visibility = 'hidden'
+    remained.style.height = 0
+  } else {
+    remained.style.visibility = 'visible'
+    liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+  }
+}
+updateBigCup();
